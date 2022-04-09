e0b0d6a30978fd4b0ea3a94419cdb3fc39204958
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2022-04-09 20:37:52 +0500

    Thumbnail provided

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,132 @@
-import React from 'react';
+import React, { useRef } from "react";
 import {
-    View,
-    Text,
-    TouchableOpacity
-} from 'react-native';
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  ImageBackground,
+  Animated,
+  ScrollView,
+} from "react-native";
+import { dummyData, COLORS, SIZES, FONTS, icons, images } from "../constants";
 
 const Home = ({ navigation }) => {
+  const newSeasonScrollX = useRef(new Animated.Value(0)).current;
+
+  function renderHeader() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("MovieDetail")}
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingHorizontal: SIZES.padding,
+        }}
+      >
+        {/* Profile */}
+        <TouchableOpacity
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            width: 50,
+            height: 50,
+          }}
+        ></TouchableOpacity>
+        {/* Screen Mirror */}
+        <TouchableOpacity
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            width: 50,
+            height: 50,
+          }}
+          onPress={() => console.log("ScreenMirror")}
+        >
+          <Image
+            source={icons.airplay}
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: COLORS.primary,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function renderNewSeasonSection() {
+    return (
+      <Animated.FlatList
+        horizontal
+        pagingEnabled
+        snapToAlignment="center"
+        snapToInterval={SIZES.width}
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        decelerationRate={0}
+        contentContainerStyle={{
+          marginTop: SIZES.radius,
+        }}
+        data={dummyData.newSeason}
+        keyExtractor={(item, index) => item.id.toString()}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: newSeasonScrollX } } }],
+          { useNativeDriver: false }
+        )}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableWithoutFeedback
+              onPress={() =>
+                navigation.navigate("MovieDetail", {
+                  selectedMovie: item,
+                })
+              }
             >
-                <Text>Navigate to MovieDetail</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+              <View
+                style={{
+                  width: SIZES.width,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {/* Thumbnail */}
+                <ImageBackground
+                  source={item.thumbnail}
+                  resizeMode="cover"
+                  style={{
+                    width: SIZES.width * 0.85,
+                    height: SIZES.width * 0.85,
+                    justifyContent: "flex-end",
+                  }}
+                ></ImageBackground>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        }}
+      />
+    );
+  }
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.black,
+      }}
+    >
+      {renderHeader()}
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {renderNewSeasonSection()}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
